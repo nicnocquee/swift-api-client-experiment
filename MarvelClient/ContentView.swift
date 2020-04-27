7b3e74330d76ea30d0835d37e81fc484e26844f2
Nico Prananta <nicnocquee@users.noreply.github.com>
2020-04-27 16:07:30 +0200

    Initial Commit

---
+++ MarvelClient/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MarvelClient
+//
+//  Created by Nico Prananta on 2020/04/27.
+//  Copyright © 2020 nico.fyi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
